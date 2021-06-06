50771119ca61077259894d65bda8403d4d163bbe
aungmyat12 <aungmyatoo2580@gmail.com>
2021-06-07 01:32:22 +0630

    7-Project-Setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <TheHeader></TheHeader>
+  <div class="container">
+    <CalculateTransaction></CalculateTransaction>
+    <transactions-list></transactions-list>
+    <add-transaction></add-transaction>
+  </div>
+</template>
+
+<script>
+import AddTransaction from "./components/AddTransaction.vue";
+import CalculateTransaction from "./components/CalculateTransaction.vue";
+import TheHeader from "./components/Layouts/TheHeader.vue";
+import TransactionsList from "./components/TransactionsList.vue";
+export default {
+  name: "App",
+  components: {
+    TheHeader,
+    CalculateTransaction,
+    TransactionsList,
+    AddTransaction,
+  },
+};
+</script>
+
+<style></style>
